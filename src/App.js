cadd3e5f69076d8889a5e42ec42222d233a930a1
zaky aly <zaky3ly@gmail.com>
2021-05-30 10:32:09 +0200

    importing NewsCards component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
+import NewsCards from "./components/NewsCards/NewsCards";
 import { alanKey } from "./constants";
 
 const apiKey = alanKey;
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: apiKey,
-      onCommand: ({ command }) => {
-        if (command === "testCommand") {
-          alert("this code is working");
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
         }
       },
     });
@@ -19,6 +22,7 @@ const App = () => {
   return (
     <div>
       <h1>NEWS APP</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 };
